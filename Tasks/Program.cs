3c02428172d2ab23d36be084b4e44859c5b2f8ed
Hakan <artuc.hakan@gmail.com>
2020-11-21 22:02:46 +0300

    write function

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Tasks
 {
-    class Program
+    internal class Program
     {
+        public static void Write(char c)
+        {
+            int i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(c);
+            }
+
+        }
         static void Main(string[] args)
         {
         }
