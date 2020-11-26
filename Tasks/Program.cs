26cdadddc60f9de506bd174a439e67ff9d67b53c
Hakan <Hakan@DESKTOP-Q741ILV>
2020-11-26 20:38:57 +0300

    exception handling

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -7,7 +7,22 @@ namespace Tasks
 
         static void Main(string[] args)
         {
-            WaitingForTasks.WaitTasks();
+            // ExceptionHandling.BasicHandling();
+            //try
+            //{
+            //    ExceptionHandling.IterativeHandling();
+            //}
+            //catch (AggregateException ae)
+            //{
+            //    Console.WriteLine("Some exceptions we didn't expect:");
+            //    foreach (var e in ae.InnerExceptions)
+            //    {
+            //        Console.WriteLine($" - {e.GetType()}");
+            //    }
+            //}
+
+            ExceptionHandling.UnobservedTaskException();
+
 
             Console.WriteLine("Main program done");
             Console.ReadKey();
