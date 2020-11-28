389094a7a66ea6f71d864f47a63cd87b64e4b1e7
Hakan Artuç <artuc.hakan@gmail.com>
2020-11-28 20:44:52 +0300

    balance test

--- DataSharingSynchronization/Program.cs
+++ DataSharingSynchronization/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataSharingSynchronization
 {
@@ -10,6 +6,10 @@ namespace DataSharingSynchronization
     {
         static void Main(string[] args)
         {
+            BankAccount.TestBalance();
+
+            Console.ReadKey();
         }
+
     }
 }
