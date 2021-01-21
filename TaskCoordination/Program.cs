b0695214881d43f920328105fce3e01ca6f66585
hakanartuc <artuc.hakan@gmail.com>
2021-01-21 20:55:09 +0300

    CreatingContinuationsDemo

---
+++ TaskCoordination/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TaskCoordination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CreatingContinuationsDemo.ContinueWhen();
+            CreatingContinuationsDemo.SimpleContinuation();
+
+            Console.ReadLine();
+        }
+    }
+}
